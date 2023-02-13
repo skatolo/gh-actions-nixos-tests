edcf3145cade87c6e859eaa5575084299eddd959
Han Verstraete <welteki@pm.me>
2023-02-13 22:03:46 +0100

    Verify kvm

---
+++ verify.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+has_apt_get() {
+  [ -n "$(command -v apt-get)" ]
+}
+
+check_kvm() {
+    if ! [ -e /dev/kvm ]; then
+        echo "KVM not found, is virtualisation available?"
+        echo "sudo modprobe kvm"
+        exit 1
+    fi
+
+    if $(has_apt_get); then
+
+        if ! command -v kvm-ok &> /dev/null; then 
+            $SUDO apt update && sudo apt install -qy cpu-checker
+        fi
+
+        $SUDO kvm-ok
+    fi
+}
+
+check_kvm
